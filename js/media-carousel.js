b5ca7721c1577c1376853a0973055d1662a9fb75
composer2 <composer2@abv.bg>
2017-02-02 18:13:25 +0200

    blog page almost done

--- js/media-carousel.js
+++ js/media-carousel.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
         pause: true,
         interval: 10000,
     });
+    // breadcrumbs
+    let href = window.location.pathname;
+    let links = href.split('/').filter(String);
+    for (link of links) {
+        $(".breadcrumb").append("<li><a href=" + link + ">" + link + "</a></li>");
+    }
+    // breadcrumbs
 });
