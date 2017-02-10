c17290ac16428e0b16236bc2135634ad18f07c90
composer2 <composer2@abv.bg>
2017-02-10 22:47:33 +0200

    added snowboard gadjets

--- js/media-carousel.js
+++ js/media-carousel.js
@@ -22,4 +22,29 @@ $(document).ready(function() {
         $(this).html(text);
     });
     //sign in/out
+    //pros n con labels
+
+    $(".cons").click(function() {
+        $(this).addClass("active");
+        $(this).prev().removeClass("active");
+        $(this).parent().parent().next().find(".tab-pros").removeClass("active")
+        $(this).parent().parent().next().find(".tab-cons").addClass("active")
+    });
+    $(".pros").click(function() {
+        $(this).addClass("active");
+        $(this).next().removeClass("active");
+        $(this).parent().parent().next().find(".tab-cons").removeClass("active")
+        $(this).parent().parent().next().find(".tab-pros").addClass("active")
+    });
+    //pros n con labels
+    //controlers thumbs
+     $(".l").click(function() {
+        $(this).css("border","3px solid red");
+        $(this).next().next().css("borderColor","#DDDDDD");
+    });
+     $(".r").click(function() {
+        $(this).css("border","3px solid green");
+        $(this).prev().prev().css("borderColor","#DDDDDD");
+    });
+    //controlers thumbs
 });
